7e9169c2439af21b5083b4a27d047bb8d036f14f
edoatleynewday <ed.oatley@newday.co.uk>
2024-04-12 08:46:23 +0000

    Fix integration tests

--- app/src/itest/java/uk/org/edoatley/HelloITest.java
+++ app/src/itest/java/uk/org/edoatley/HelloITest.java
@@ -1,28 +1,19 @@
 package uk.org.edoatley;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.IOException;
-
-import org.eclipse.jetty.http.HttpStatus;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
 
 import uk.org.edoatley.config.ITestConfiguration;
 
 public class HelloITest {
     private static final Logger log = LoggerFactory.getLogger(HelloITest.class);
-    private static final String API_HELLO = "/api/hello/";
+    private static final String API_HELLO = "/api/hello";
     private String serviceUrl;
 
     @BeforeEach
@@ -32,27 +23,30 @@ public class HelloITest {
         if (serviceUrl != null && serviceUrl.endsWith("/")) {
             serviceUrl = serviceUrl.substring(0, serviceUrl.length() - 1);
         }
+        log.info("Initialised HelloITest using the serviceUrl: {}", serviceUrl);
     }
 
     @Test
-    void testHello() throws ClientProtocolException, IOException {
-        // Given
+    @DisplayName("Call the /hello endpoint")
+    public void testSimpleHello() {
+        when()
+                .get(serviceUrl + API_HELLO)
+                .then()
+                .statusCode(200)
+                .contentType(equalTo("application/json"))
+                .body("message", equalTo("Howdy!"));
+
+    }
+
+    @Test
+    @DisplayName("Call the /api//hello endpoint with a name parameter")
+    public void testNamedHello() {
         String name = RandomStringUtils.randomAlphabetic(8);
-        String servicerUrl = serviceUrl + API_HELLO + name;
-        log.info("serviceUrl: " + servicerUrl);
-        HttpUriRequest request = new HttpGet(servicerUrl);
-        request.setHeader("Content-Type", "application/json");
-
-        // When
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        CloseableHttpResponse httpResponse = client.execute(request);
-
-        // Then
-        log.debug("Response: {}", httpResponse.toString());
-        log.debug("Status Line: {}", httpResponse.getStatusLine().toString());
-        log.debug("Entity: {}", httpResponse.getEntity().toString());
-
-        assertEquals(HttpStatus.OK_200, httpResponse.getStatusLine().getStatusCode());
-        assertEquals(new BasicResponseHandler().handleResponse(httpResponse), "Hello " + name);
+        when()
+                .get(serviceUrl + API_HELLO + "/" + name)
+                .then()
+                .statusCode(200)
+                .contentType(equalTo("application/json"))
+                .body("message", equalTo("Hello " + name));
     }
 }
