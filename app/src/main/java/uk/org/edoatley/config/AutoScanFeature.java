bea0272f761b45069642c874f344f262cafcff85
edoatleynewday <ed.oatley@newday.co.uk>
2024-04-24 19:55:49 +0100

    working with injection based on metadata

--- app/src/main/java/uk/org/edoatley/config/AutoScanFeature.java
+++ app/src/main/java/uk/org/edoatley/config/AutoScanFeature.java
@@ -7,27 +7,31 @@ import org.glassfish.hk2.api.Populator;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ClasspathDescriptorFileFinder;
 import org.glassfish.hk2.utilities.DuplicatePostProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Feature;
 import jakarta.ws.rs.core.FeatureContext;
 
 public class AutoScanFeature implements Feature {
 
+    private static final Logger log = LoggerFactory.getLogger(AutoScanFeature.class);
+
     @Inject
     ServiceLocator serviceLocator;
 
     @Override
     public boolean configure(FeatureContext context) {
-
+        log.info("AutoScanFeature configure");
         DynamicConfigurationService dcs =
                 serviceLocator.getService(DynamicConfigurationService.class);
         Populator populator = dcs.getPopulator();
-        // Populator - populate HK2 service locators from inhabitants files
-        // ClasspathDescriptorFileFinder - find files from META-INF/hk2-locator/default
+
         try {
             populator.populate(new ClasspathDescriptorFileFinder(this.getClass().getClassLoader()),
                     new DuplicatePostProcessor());
         } catch (MultiException | IOException e) {
+            log.error("Error populating HK2 service locators", e);
             throw new RuntimeException(e);
         }
         return true;
--- app/src/main/java/uk/org/edoatley/config/JettyResourceConfig.java
+++ app/src/main/java/uk/org/edoatley/config/JettyResourceConfig.java
@@ -1,10 +1,7 @@
 package uk.org.edoatley.config;
 
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import uk.org.edoatley.security.AuthenticationFilter;
-import uk.org.edoatley.security.idp.IdentityProvider;
-import uk.org.edoatley.security.idp.impl.DummyIdentityProvider;
 
 public class JettyResourceConfig extends ResourceConfig {
     public JettyResourceConfig() {
@@ -15,13 +12,7 @@ public class JettyResourceConfig extends ResourceConfig {
         // Register Auth Filter here
         register(AuthenticationFilter.class);
 
-        // register(AutoScanFeature.class);
-
-        register(new AbstractBinder() {
-            @Override
-            protected void configure() {
-                bind(DummyIdentityProvider.class).to(IdentityProvider.class);
-            }
-        });
+        // Register HK2 scanning
+        register(AutoScanFeature.class);
     }
 }
