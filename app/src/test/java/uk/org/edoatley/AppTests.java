5b62b0b173d7da8f1164a66c4de749736be4bd24
edoatleynewday <ed.oatley@newday.co.uk>
2024-04-25 10:55:11 +0100

    fix test attempt 2

--- app/src/test/java/uk/org/edoatley/AppTests.java
+++ app/src/test/java/uk/org/edoatley/AppTests.java
@@ -6,6 +6,7 @@ import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
+import org.apache.commons.lang3.RandomStringUtils;
 
 /**
  * Thiese tests are the core tests we can run in 3 modes - HTTP, secured with TLS and integration.
@@ -36,17 +37,18 @@ public abstract class AppTests {
     @Test
     @DisplayName("Call the /hello endpoint with a name parameter")
     public void testNamedHello() {
+        String name = RandomStringUtils.randomAlphabetic(8);
         // @formatter:off
         login();
 
         given().
             header("Authorization", "Bearer specialtoken").
         when().
-            get(API_HELLO + "/Bob").
+            get(API_HELLO + "/" + name).
         then().
             statusCode(200).
             contentType(equalTo("application/json")).
-            body("message", equalTo("Hello Bob"));
+            body("message", equalTo("Hello " + name));
         // @formatter:on
     }
 
