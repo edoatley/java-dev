acae95af26ab9ad9a9c1d47d7e206c7c6ca854da
edoatley <edoatley@gmail.com>
2024-05-10 17:55:29 +0100

    write up GH authN

--- iac/azure/static/main.tf
+++ iac/azure/static/main.tf
@@ -38,6 +38,7 @@ resource "azurerm_network_security_group" "this" {
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 }
+
 resource "azurerm_subnet_network_security_group_association" "this" {
   for_each                  = azurerm_subnet.subnets
   subnet_id                 = each.value.id
