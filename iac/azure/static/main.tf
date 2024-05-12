9d64c95063e8ee4f871f427d4e965dd8b4eef2e6
edoatley <edoatley@gmail.com>
2024-05-12 15:08:35 +0100

    Add version

--- iac/azure/static/main.tf
+++ iac/azure/static/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network" "this" {
 }
 
 resource "azurerm_subnet" "subnets" {
-  for_each             = var.subnets
+  for_each             =                 var.subnets
   name                 = each.value.name
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
